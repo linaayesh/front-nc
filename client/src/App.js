bb5511ac9e24a88ccc92806cb807ab1a76b051e3
afahgan <afahgan@students.iugaza.edu.ps>
2022-06-28 16:29:31 +0300

    Setup React

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import './index.css';
+import { Routes, Route } from 'react-router-dom';
+
+import './app.css';
+import Home from './Layouts';
+import HomePage from './Pages';
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route index element={<HomePage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
