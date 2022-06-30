f9bbf613c7310bb3df6018c1f184ee1ed981fa05
afahgan <afahgan@students.iugaza.edu.ps>
2022-06-30 09:28:55 +0300

    edit dashboard

--- client/src/Components/Dashboard/SideBar/index.jsx
+++ client/src/Components/Dashboard/SideBar/index.jsx
@@ -3,36 +3,53 @@ import './style.css';
 import { Link } from 'react-router-dom';
 
 function SideBar() {
+  const links = [
+    {
+      name: 'Home',
+      link: '/dashboard',
+      icon: 'fa fa-home icon',
+    },
+    {
+      name: 'Dashboard',
+      link: '/dashboard',
+      icon: 'fa fa-dashboard icon',
+    },
+    {
+      name: 'Finance',
+      link: '/dashboard/finance',
+      icon: 'fa fa-user icon',
+    },
+    {
+      name: 'Account Settings',
+      link: '/dashboard/account-settings',
+      icon: 'fa fa-cog icon',
+    },
+    {
+      name: 'Dashobard Settings',
+      link: '/dashboard/Dashobard-settings',
+      icon: 'fa fa-cog icon',
+    },
+    {
+      name: 'Logout',
+      link: '/logout',
+      icon: 'fa fa fa-sign-out icon',
+    },
+  ];
+
   return (
     <div className="sidebar">
 
       <div className="sidebar-body">
-        <div className="sidebar-body-item">
-          <i className="fa fa-home icon" />
-          Home
-        </div>
-        <div className="sidebar-body-item">
-          <i className="fa fa-dashboard icon" />
-          Dashobard
+        {links.map((link, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <Link to={link.link} key={index}>
+            <div className="sidebar-body-item">
+              <i className={link.icon} />
+              <span>{link.name}</span>
+            </div>
+          </Link>
+        ))}
 
-        </div>
-        <div className="sidebar-body-item">
-          <i className="fa fa-user icon" />
-          Finance
-        </div>
-        <div className="sidebar-body-item">
-          <i className="fa fa-cog icon" />
-          Account Settings
-        </div>
-        <div className="sidebar-body-item">
-          <i className="fa fa-cog icon" />
-          Dashobard Settings
-        </div>
-        <div className="sidebar-body-item">
-          <i className="fa fa-sign-out icon" />
-          <Link to="/">Logout</Link>
-
-        </div>
       </div>
 
     </div>
