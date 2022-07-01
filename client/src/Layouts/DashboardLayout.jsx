5b2ab7c6decd29cdf8a26a5a646224b1ff496069
afahgan <afahgan@students.iugaza.edu.ps>
2022-07-01 19:12:17 +0300

    update dashboard layout

--- client/src/Layouts/DashboardLayout.jsx
+++ client/src/Layouts/DashboardLayout.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Navbar } from '../Components';
+import Sidebar from '../Components/Dashboard';
 
 export default function DashboardLayout() {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <div className="both">
+        <div className="left">
+          <Sidebar />
+        </div>
+        <div className="right">
+          <Outlet />
+
+        </div>
+      </div>
     </>
   );
 }
