119ecb62cefcb4a172001daf8af4e0025cef514c
afahgan <afahgan@students.iugaza.edu.ps>
2022-07-02 15:11:37 +0300

    remove upload button from navbar

--- client/src/Components/Navbar/index.jsx
+++ client/src/Components/Navbar/index.jsx
@@ -12,12 +12,7 @@ function Navbar() {
 
         </div>
         <div className="rightside">
-
-          <button type="button" className="navbar-button">
-            <i className="fa fa-plus icon" />
-            Upload Video
-
-          </button>
+          <i className="fa fa-bell icon" />
 
           <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80" alt="logo" className="userimg" />
 
