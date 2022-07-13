11047327ed99aa18a2909eeb9ab95edffd0e8164
afahgan <afahgan@students.iugaza.edu.ps>
2022-07-13 11:23:11 +0300

    update style

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -5,26 +5,21 @@ import { Button } from 'antd';
 
 function Navbar() {
   return (
-    <>
-      {/* Create navbar */}
-      <nav className="navbar">
-        <div className="leftside">
-          <div className="bti">
-            <Button>
-              <i className="fa fa-bars" />
-            </Button>
-          </div>
-          {/* Create rightside */}
+    <nav className="navbar">
+      <div className="leftside">
+        <div className="bti">
+          <Button>
+            <i className="fa fa-bars" />
+          </Button>
         </div>
-        <div className="rightside">
-          <i className="fa fa-bell icon" />
+      </div>
+      <div className="rightside">
+        <i className="fa fa-bell icon" />
 
-          <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80" alt="logo" className="userimg" />
+        <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80" alt="logo" className="userimg" />
 
-        </div>
-      </nav>
-
-    </>
+      </div>
+    </nav>
   );
 }
 
