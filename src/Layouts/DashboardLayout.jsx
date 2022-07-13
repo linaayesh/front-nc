8bbaaa6abd1f2083326e9cbdf6b172ae1bb1df93
afahgan <afahgan@students.iugaza.edu.ps>
2022-07-13 11:10:49 +0300

    update some stuffs

--- src/Layouts/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout.jsx
@@ -4,20 +4,17 @@ import { Navbar, SideBar } from '../Components';
 
 export default function DashboardLayout() {
   return (
-    <div className="asdoasd">
-      <div className="both">
-        <div className="sidebarinlayout">
+    <div className="both">
+      <div className="sidebarinlayout">
 
-          <SideBar />
-        </div>
-        <div className="navbarinlayout">
-          <Navbar />
-          <div>
-            <Outlet />
-          </div>
+        <SideBar />
+      </div>
+      <div className="navbarinlayout">
+        <Navbar />
+        <div>
+          <Outlet />
         </div>
       </div>
     </div>
-
   );
 }
