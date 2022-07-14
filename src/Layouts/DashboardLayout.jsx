d017d502adc6855dc9c009e6a84a17c9601b18af
afahgan <afahgan@students.iugaza.edu.ps>
2022-07-14 08:42:47 +0300

    make sidebar responisive

--- src/Layouts/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Navbar, SideBar } from '../Components';
 
 export default function DashboardLayout() {
+  const sideBarInLayout = useRef(null);
+  const navbarInLayout = useRef(null);
+  const handleClick = () => {
+    const navbar = sideBarInLayout.current.style.display;
+    if (navbar === 'none') {
+      sideBarInLayout.current.style.display = 'block';
+      sideBarInLayout.current.style.width = '100%';
+      navbarInLayout.current.style.display = 'none';
+    } else {
+      navbarInLayout.current.style.display = 'block';
+
+      sideBarInLayout.current.style.display = 'none';
+    }
+  };
+
   return (
     <div className="both">
-      <div className="sidebarinlayout">
-
-        <SideBar />
+      <div ref={sideBarInLayout} className="sidebarinlayout">
+        <SideBar handleClick={handleClick} />
       </div>
-      <div className="navbarinlayout">
-        <Navbar />
-        <div>
+      <div ref={navbarInLayout} className="navbarinlayout">
+        <Navbar handleClick={handleClick} />
+        <div className="Outletstyle">
           <Outlet />
         </div>
       </div>
