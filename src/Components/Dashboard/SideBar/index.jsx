f88077f487bbbdeb43fb27c18f6f7ecd468619a0
afahgan <afahgan@students.iugaza.edu.ps>
2022-07-15 19:22:29 +0300

    remove eslint comment

--- src/Components/Dashboard/SideBar/index.jsx
+++ src/Components/Dashboard/SideBar/index.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-nested-ternary */
-
 import React, { useEffect, useState } from 'react';
 import { CloseOutlined } from '@ant-design/icons';
 import PropTypes from 'prop-types';
