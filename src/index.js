b14cc66e8e16f8010764643278873179f127d763
afahgan <afahgan@students.iugaza.edu.ps>
2022-07-17 12:37:31 +0300

    use Provider in index file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './Store';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </StrictMode>,
 );
