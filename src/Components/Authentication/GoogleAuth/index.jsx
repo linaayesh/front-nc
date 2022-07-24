e811007c103a966388df11cf60279d6f5d5d1032
Rand <randsohail98@gmail.com>
2022-07-24 15:46:07 +0300

    remove console

--- src/Components/Authentication/GoogleAuth/index.jsx
+++ src/Components/Authentication/GoogleAuth/index.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import React, { useEffect } from 'react';
 import {
   message,
@@ -19,20 +18,19 @@ export default function GoogleAuth({ label }) {
     };
     gapi.load('client:auth2', start);
   }, []);
+
   const successResponse = async (response) => {
     const { tokenId } = response;
     try {
-      const googleResponse = await axios.post('/api/v1/auth/sign/google', { tokenId });
-      console.log(googleResponse.data.message);
+      const responseAPI = await axios.post('/api/v1/auth/sign/google', { tokenId });
+      console.log('response', responseAPI.data);
     } catch (error) {
-      console.log(22222, error.response.data.message);
-      // message.error(error.response.data.message);
+      message.warning(error.response.data.message);
     }
   };
 
   const failureResponse = (response) => {
-    console.log('error', response);
-    message.error(response); // something went wrong try a gain later
+    message.error(response);
   };
   return (
     <GoogleLogin
@@ -42,7 +40,6 @@ export default function GoogleAuth({ label }) {
       onSuccess={successResponse}
       onFailure={failureResponse}
       cookiePolicy="single_host_origin"
-      isSignedIn
     />
 
   );
