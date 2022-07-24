0e7fc623fe60c10d2de88bd91b9267e3b9c720cf
afahgan <afahgan@students.iugaza.edu.ps>
2022-07-24 16:09:58 +0300

    Create The Component and style it

---
+++ src/Components/Dashboard/EditProfile/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './style.css';
+
+function EditProfile() {
+  return (
+    <div className="eidtProfileDiv">
+      <div className="editProfileHeader">
+        <h1>Edit Profile</h1>
+        <div className="container">
+          <div className="editnav">
+            <div className="editicon" />
+            <div className="nameeidt">
+              Name & description
+            </div>
+          </div>
+          <div className="editPageContent">
+            flex
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EditProfile;
