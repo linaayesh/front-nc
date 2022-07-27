4bebe06ce1dce16da56efe01fd9d5aa5f67c1156
afahgan <afahgan@students.iugaza.edu.ps>
2022-07-27 12:49:03 +0300

    Update edit profile page

--- src/Components/Dashboard/EditProfile/index.jsx
+++ src/Components/Dashboard/EditProfile/index.jsx
@@ -1,22 +1,95 @@
 import React from 'react';
-
 import './style.css';
+// import { Switch } from 'antd';
+import {
+  Input, Button, Form, message,
+} from '../../AntDesign';
+
+import axiosCall from '../../../Services/ApiCall';
 
 function EditProfile() {
+  const [form] = Form.useForm();
+  // const onChange = (checked) => {
+  //   console.log(`switch to ${checked}`);
+  // };
+  const onFinish = async (values) => {
+    const { name } = values;
+    console.log(name);
+    try {
+      const response = await axiosCall('/api/v1/auth/editProfile', 'post', { name });
+      message.success(response.data.message);
+      form.resetFields();
+    } catch (error) {
+      message.error(error.response.data.message);
+    }
+  };
   return (
     <div className="eidtProfileDiv">
       <div className="editProfileHeader">
         <h1>Edit Profile</h1>
         <div className="container">
-          <div className="editnav">
-            <div className="editicon" />
-            <div className="nameeidt">
-              Name & description
+
+          <Form
+            form={form}
+            name="basic"
+            onFinish={onFinish}
+            autoComplete="off"
+            className="edit-form"
+          >
+            <div className="edit-nav">
+              <div className="edit-icon" />
+              <div className="title-eidt">Change Your Information</div>
             </div>
-          </div>
-          <div className="editPageContent">
-            flex
-          </div>
+
+            <div className="ayman">
+              <div className="ayman2">
+                <Form.Item
+                  name="name"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please enter your name!',
+                    },
+                  ]}
+                >
+                  <Input placeholder="Name" />
+                </Form.Item>
+                {/* <Form.Item
+                  name="email"
+                  rules={[
+                    {
+                      type: 'email',
+                      required: true,
+                      message: 'Please enter a vaild email!',
+                    },
+                  ]}
+                >
+                  <Input placeholder="Email" type="email" />
+                </Form.Item> */}
+              </div>
+              <div className="ayman3">upload</div>
+
+            </div>
+            {/* <div className="edit-nav">
+              <div className="edit-icon" />
+              <div className="title-eidt">Notification</div>
+
+            </div>
+            <div className="edit-switch">
+              Video Approval
+              <Switch
+                defaultChecked
+                onChange={onChange}
+
+              />
+            </div> */}
+            <Form.Item>
+              <Button className="edit-button" type="primary" htmlType="submit">
+                Sign Up
+              </Button>
+            </Form.Item>
+          </Form>
+
         </div>
       </div>
     </div>
