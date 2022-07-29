f8fe0e9fd2b4b0f8876d367a28f836264a58ca15
Jood80 <shawar.nujood@gmail.com>
2022-07-29 14:28:19 +0300

    feat: create upload image component

---
+++ src/Components/Dashboard/EditProfile/uploadImage/index.jsx
@@ -0,0 +1,112 @@
+import { useState } from 'react';
+import PropsTypes from 'prop-types';
+import {
+  PlusOutlined, LoadingOutlined, DeleteOutlined,
+} from '@ant-design/icons';
+import {
+  Button,
+  message, Upload,
+  Avatar,
+} from 'antd';
+import ImgCrop from 'antd-img-crop';
+
+import { DEFAULT_AVATAR } from '../../../../Constants';
+
+const getBase64 = (img, callback) => {
+  const reader = new FileReader();
+  reader.addEventListener('load', () => callback(reader.result));
+  reader.readAsDataURL(img);
+};
+
+const beforeUpload = (file) => {
+  console.log('before upload - file', file);
+  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+
+  if (!isJpgOrPng) {
+    message.error('Only JPG/PNG images are accepted!');
+  }
+
+  // is less than 2MB
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error('Image must be smaller than 2MB!');
+  }
+
+  return isJpgOrPng && isLt2M;
+};
+
+function ImageUploader() {
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState(DEFAULT_AVATAR);
+
+  const handleChange = (info) => {
+    if (info.file.status === 'uploading') {
+      setLoading(true);
+    }
+
+    if (info.file.percent === 100) {
+      getBase64(info.file.originFileObj, (url) => {
+        setLoading(false);
+        setImageUrl(url);
+      });
+    }
+  };
+
+  const handleDelete = () => {
+    setImageUrl('');
+  };
+
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div
+        style={{
+          marginTop: 8,
+        }}
+      >
+        Upload
+      </div>
+    </div>
+  );
+
+  const deleteButton = (
+    <div>
+      {imageUrl && (
+        <Button danger icon={<DeleteOutlined />} size="medium" onClick={handleDelete}>
+          Remove
+        </Button>
+      )}
+    </div>
+  );
+
+  return (
+    <div>
+      <ImgCrop beforeCrop={beforeUpload} rotate shape="round">
+        <Upload
+          name="avatar"
+          listType="picture-card"
+          className="avatar-uploader"
+          showUploadList={false}
+          beforeUpload={beforeUpload}
+          onChange={handleChange}
+        >
+          {imageUrl ? (
+            <div className="avatar-image">
+              <Avatar
+                shape="circle"
+                src={imageUrl}
+                alt="avatar"
+                style={{ width: '100%', height: '100%' }}
+              />
+            </div>
+          ) : (
+            uploadButton
+          )}
+        </Upload>
+      </ImgCrop>
+      {imageUrl && deleteButton}
+    </div>
+  );
+}
+
+export default ImageUploader;
---
+++ src/Constants/index.js
@@ -0,0 +1,3 @@
+export const DEFAULT_AVATAR = 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80';
+
+export const ANONYMOUS_USER = 'anonymous';
