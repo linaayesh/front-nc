359516a19dfd27ee69793ac1ae40f7f64a16c451
Jood80 <shawar.nujood@gmail.com>
2022-08-23 20:37:39 +0300

    refactor: use dispatch in googleAuth

--- src/components/Authentication/GoogleAuth/index.jsx
+++ src/components/Authentication/GoogleAuth/index.jsx
@@ -5,11 +5,9 @@ import { gapi } from 'gapi-script';
 import { useNavigate } from 'react-router-dom';
 
 import { useAppDispatch } from 'hooks';
-import { userService } from 'services';
-import { getUser } from 'store/auth/thunk';
+import { googleLogin } from 'store/auth/thunk';
 import { message } from 'components/AntDesign';
 import { CLIENT_ID } from 'shared/constants/config';
-import { HTTP_EXCEPTIONS_MESSAGES } from 'shared/constants';
 
 export default function GoogleAuth({ label, method }) {
   const navigate = useNavigate();
@@ -27,31 +25,15 @@ export default function GoogleAuth({ label, method }) {
 
   const successResponse = async (response) => {
     const { tokenId } = response;
-    try {
-      const googleResponse = await userService.googleLogin(method, tokenId);
 
-      if (googleResponse.status === 201) {
-        message.success(HTTP_EXCEPTIONS_MESSAGES[googleResponse.data.message]);
-      }
-      if (googleResponse.status === 200) {
-        message.success(HTTP_EXCEPTIONS_MESSAGES[googleResponse.data.message]);
-        navigate('/dashboard');
-
-        dispatch(getUser());
-      }
-    } catch (error) {
-      console.log(error);
-      if (error.toString().includes('NOT EXIST USER')) {
-        message.error(HTTP_EXCEPTIONS_MESSAGES['NOT EXIST USER']);
-      } else if (error.toString().includes(' ALREADY APPROVED')) {
-        message.error(HTTP_EXCEPTIONS_MESSAGES['APPROVED ACCOUNT']);
-      }
-    }
+    await dispatch(googleLogin({ method, tokenId })).unwrap();
+    navigate('/dashboard');
   };
 
   const failureResponse = (response) => {
     message.error(response);
   };
+
   return (
     <GoogleLogin
       className="google-sign-up-button"
