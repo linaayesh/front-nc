17b2fc9e7f0f3600cfea7963c8e800bb04ca1809
Ibarhim-Jarada <ibrahim.jarada@gmail.com>
2022-08-29 13:55:19 +0300

    To redirect logged users to the same page asked before route protection check. Relates #83

--- src/middleware/ProtectLogin.jsx
+++ src/middleware/ProtectLogin.jsx
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 import { useAppSelector } from 'hooks';
 
 export default function Pro({ children }) {
   const { isLoading, isLoggedIn } = useAppSelector(({ checkAuth }) => checkAuth);
-
+  const { state } = useLocation();
   if (!isLoading && isLoggedIn) {
-    return <Navigate to="/dashboard" />;
+    return <Navigate to={state?.path || '/dashboard'} />;
   }
 
   return children;
