7b386743f09863c5a885af98ae4144f1a08d36c3
AymanSami98 <afahgan@students.iugaza.edu.ps>
2022-08-30 14:05:10 +0300

    Move the dashboard settings to admin side insted of common

--- src/pages/UserPages/Admin/index.js
+++ src/pages/UserPages/Admin/index.js
@@ -3,3 +3,4 @@ export { default as RejectedUsersPage } from './RejectedUsers.page';
 export { default as UserListPage } from './UserList.page';
 export { default as AddNewUserPage } from './AddNewUser.page';
 export { default as ContentListPage } from './ContentList.page';
+export { default as DashboardSettingsPage } from './DashboardSettings.page';
--- src/pages/UserPages/Common/index.js
+++ src/pages/UserPages/Common/index.js
@@ -1,3 +1,2 @@
 export { default as ChangePasswordPage } from './ChangePassword.page';
 export { default as EditProfilePage } from './EditProfile.page';
-export { default as DashboardSettingsPage } from './DashboardSettings.page';
