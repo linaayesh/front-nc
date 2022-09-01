5364f313440286b435d4bd88328f974ba749c9ba
AymanSami98 <afahgan@students.iugaza.edu.ps>
2022-09-01 14:00:51 +0300

    send request to update data

--- src/components/Dashboard/DashboardSettings/index.jsx
+++ src/components/Dashboard/DashboardSettings/index.jsx
@@ -1,66 +1,118 @@
+import { useAppDispatch } from 'hooks';
 import { Input, Button, Form } from 'components/AntDesign';
 import { validationMessages } from 'utils';
 import './style.css';
+import { updateDashboardSettings } from 'store/admin/thunk';
 
 function DashboardSettings() {
   const [form] = Form.useForm();
+  const dispatch = useAppDispatch();
+  const onFinish = (values) => {
+    const {
+      viewliftPassword,
+      viewliftEndpoint,
+      viewliftWatchesFetchLimit,
+      systemActivationDate,
+      nextupToOwedSplitPercentage,
+      expiredAfterInYears,
+      fetchMaxCount,
+    } = values;
+    const viewliftEmail = values.viewliftEmail.toLowerCase();
+    dispatch(updateDashboardSettings({
+      viewliftEmail,
+      viewliftPassword,
+      viewliftEndpoint,
+      viewliftWatchesFetchLimit,
+      systemActivationDate,
+      nextupToOwedSplitPercentage,
+      expiredAfterInYears,
+      fetchMaxCount,
+    }));
+  };
 
   return (
     <div className="dash-settings-hero">
 
       <Form
+        onFinish={onFinish}
         form={form}
         name="basic"
+        layout="vertical"
+        requiredMark={false}
         autoComplete="off"
         className="dash-settings-form"
       >
 
         <div className="dash-settings-sub">
           <div className="dash-settings-right">
+
             <Form.Item
-              className="dash-settings-input"
+              label="View lift Email"
               name="viewliftEmail"
-              rules={validationMessages.VIEWLLIFT_EMAIL}
+              className="dash-settings-input"
+              rules={[{ required: true, message: validationMessages.viewliftEmail }]}
             >
-              <Input placeholder="Enter The New Email" type="text" />
+              <Input placeholder="Viewlift Email" type="email" />
             </Form.Item>
             <Form.Item
               className="dash-settings-input"
+              label="View lift Password"
               name="viewliftPassword"
-              rules={validationMessages.VIEWLLIFT_PASSWORD}
+              rules={[{ required: true, message: validationMessages.viewliftPassword }]}
             >
-              <Input placeholder="Enter The New Password" type="password" />
+              <Input.Password placeholder="Viewlift Password" type="password" />
             </Form.Item>
             <Form.Item
               className="dash-settings-input"
+              label="View lift Endpoint"
               name="viewliftEndpoint"
-              rules={validationMessages.VIEWLIFT_ENDPOINT}
+              rules={[{ required: true, message: validationMessages.viewliftEndpoint }]}
             >
-              <Input placeholder="Enter The New Endpoint" type="text" />
+              <Input placeholder="Viewlift Endpoint" type="url" />
             </Form.Item>
+            <Form.Item
+              className="dash-settings-input"
+              label="View lift Watches Fetch Limit"
+              name="viewliftWatchesFetchLimit"
+              rules={[{ required: true, message: validationMessages.viewliftWatchesFetchLimit }]}
+            >
+              <Input placeholder="Viewlift Watches Fetch Limit" type="number" />
+            </Form.Item>
+
           </div>
           <div className="dash-settings-left">
 
             <Form.Item
               className="dash-settings-input"
-              name="viewliftWatchesFetchLimit"
-              rules={validationMessages.VIEWLIFT_WATCHES_FETCH_LIMIT}
+              label="System Activation Date"
+              name="systemActivationDate"
+              rules={[{ required: true, message: validationMessages.systemActivationDate }]}
             >
-              <Input placeholder="Enter The New Watches Fetch Limit" type="text" />
+              <Input placeholder="System Activating Date" type="date" />
             </Form.Item>
             <Form.Item
               className="dash-settings-input"
-              name="systemActivatingDate"
-              rules={validationMessages.SYSTEM_ACTIVATING_DATE}
+              label="Nextup To Owed Split Percentage"
+              name="nextupToOwedSplitPercentage"
+              rules={[{ required: true, message: validationMessages.nextupToOwedSplitPercentage }]}
             >
-              <Input placeholder="Enter The New Activating Date" type="text" />
+              <Input placeholder="Split Ratio" type="number" />
             </Form.Item>
             <Form.Item
               className="dash-settings-input"
-              name="splitRatio"
-              rules={validationMessages.splitRatio}
+              label="Expired After In Years"
+              name="fetchMaxCount"
+              rules={[{ required: true, message: validationMessages.fetchMaxCount }]}
             >
-              <Input placeholder="Enter The New Split Ratio" type="text" />
+              <Input placeholder="Fetch Max Count" type="number" />
+            </Form.Item>
+            <Form.Item
+              className="dash-settings-input"
+              label="Expired After In Years"
+              name="expiredAfterInYears"
+              rules={[{ required: true, message: validationMessages.expiredAfterInYears }]}
+            >
+              <Input placeholder="Expired After In Years" type="number" />
             </Form.Item>
           </div>
         </div>
