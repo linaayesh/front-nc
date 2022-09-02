1f2105092adbdcf953dc8e32a062721b33332da0
AymanSami98 <afahgan@students.iugaza.edu.ps>
2022-09-02 16:49:31 +0300

    update logo to take the classname cause it been used in deffrient places

--- src/shared/components/Logo/index.jsx
+++ src/shared/components/Logo/index.jsx
@@ -1,14 +1,20 @@
+import PropTypes from 'prop-types';
+
 import logo from 'assets/images/NextUpLogo.png';
 import './style.css';
 
-export default function Logo() {
+export default function Logo({ className }) {
   return (
     <img
       src={logo}
       alt="logo of NextUpComedy"
-      className="home-layout-logo"
+      className={className}
       loading="lazy"
       decoding="async"
     />
   );
 }
+
+Logo.propTypes = {
+  className: PropTypes.string.isRequired,
+};
