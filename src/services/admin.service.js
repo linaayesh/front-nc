444d94b281cc7996bc12e998383af12350a01f2c
AymanSami98 <afahgan@students.iugaza.edu.ps>
2022-09-06 16:59:02 +0300

    update the service

--- src/services/admin.service.js
+++ src/services/admin.service.js
@@ -12,6 +12,7 @@ import {
   MATCH_USER_CONTENT,
   UPDATE_DASHBOARD_SETTINGS,
   GET_DASHBOARD_SETTINGS,
+  EDIT_USER_PROFILE,
 } from 'shared/constants/endpoints';
 import { axiosCall } from '../utils';
 
@@ -29,4 +30,5 @@ export default {
   matchUserContent: async (payload) => axiosCall(MATCH_USER_CONTENT, 'PATCH', payload),
   updateDashboardSettings: async (payload) => axiosCall(UPDATE_DASHBOARD_SETTINGS, 'PATCH', payload),
   geteDashboardSettings: async () => axiosCall(GET_DASHBOARD_SETTINGS, 'GET'),
+  editUserProfile: async (userInfo) => axiosCall(EDIT_USER_PROFILE, 'PATCH', userInfo),
 };
