c6092b98b7ce4bd105aa5392dbe6636a40fc9b27
AymanSami98 <afahgan@students.iugaza.edu.ps>
2022-09-07 12:11:58 +0300

    give the admin abilty to update user image

--- src/components/Dashboard/EditUserData/index.jsx
+++ src/components/Dashboard/EditUserData/index.jsx
@@ -4,17 +4,28 @@ import { ComponentLayout } from 'layouts';
 import { validationMessages } from 'utils';
 import { Input, Button, Form } from 'components/AntDesign';
 import { editUserProfile, getUserProfile } from 'store/admin/thunk';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import ImageUploader from '../EditProfile/uploadImage';
 
 function EditUserData() {
   const dispatch = useAppDispatch();
+  const [image, setImage] = useState(null);
+  const [oldImage, setOldImage] = useState(null);
+  const [isFormChanged, setIsFormChanged] = useState(false);
+
   const [form] = Form.useForm();
   const { userId } = useParams();
-
+  useEffect(() => {
+    if (image) {
+      setIsFormChanged(true);
+    }
+    return () => setIsFormChanged(false);
+  }, [form, image]);
   useEffect(() => {
     (async () => {
       const { payload } = await dispatch(getUserProfile(userId));
       form.setFieldsValue(payload);
+      setOldImage(payload.image);
     })();
   }, []);
 
@@ -23,6 +34,7 @@ function EditUserData() {
     const userUpdatedInfo = {
       id: userId,
       ...(name && { name }),
+      ...image && { image },
     };
     dispatch(editUserProfile(userUpdatedInfo));
   };
@@ -33,9 +45,25 @@ function EditUserData() {
           <Form.Item name="name" rules={validationMessages.name}>
             <Input placeholder="Name" className="edit-form-input" />
           </Form.Item>
-        </div>
 
-        <Button className="edit-button-admin" type="primary" htmlType="submit">
+        </div>
+        <div className="edit-form-t">
+          <div className="heading-nav">
+            <div className="heading-icon" />
+            <div className="title-heading">Change Your Profile Pic</div>
+          </div>
+          <div className="ImageUploader">
+            <ImageUploader submitImageToForm={(url) => setImage(url)} />
+            <img
+              src={oldImage}
+              alt="logo"
+              className="profileimg"
+              loading="lazy"
+              decoding="async"
+            />
+          </div>
+        </div>
+        <Button className="edit-button-admin" type="primary" htmlType="submit" disabled={!isFormChanged}>
           Edit Profile
         </Button>
       </Form>
