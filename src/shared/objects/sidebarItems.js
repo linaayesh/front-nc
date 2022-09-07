c418aa65fc9f305240a1780d2a3b7fe7339c2543
AymanSami98 <afahgan@students.iugaza.edu.ps>
2022-09-07 22:11:15 +0300

    update the sidebar item to add an admin role

--- src/shared/objects/sidebarItems.js
+++ src/shared/objects/sidebarItems.js
@@ -94,6 +94,7 @@ const itemsSuperAdmin = [
   },
 
 ];
+
 const itemsAdmin = [
   {
     title: 'Home',
@@ -118,6 +119,91 @@ const itemsAdmin = [
         title: 'Content List',
         path: '/dashboard/contentlist',
         key: 'contentlist',
+      },
+    ],
+  },
+  {
+    title: 'Finance',
+    icon: 'fa fa-money',
+    children: [
+      {
+        title: 'Revenue',
+        path: '/dashboard/revenue',
+        key: 'revenue',
+      },
+    ],
+  },
+  {
+    title: 'User Management',
+    icon: 'fa fa-users',
+    children: [
+      {
+        title: 'User List',
+        path: '/dashboard/userlist',
+        key: 'userlist',
+      },
+
+      {
+        title: 'List of New Users',
+        path: '/dashboard/newusers',
+        key: 'newusers',
+      },
+      {
+        title: 'List of Rejected Users',
+        path: '/dashboard/rejectedusers',
+        key: 'rejectedusers',
+      },
+      {
+        title: 'Add New User',
+        path: '/dashboard/addNewUser',
+        key: 'addNewUser',
+      },
+    ],
+  },
+  {
+    title: 'Account Settings',
+    icon: 'fa fa-cog',
+
+    children: [
+      {
+        title: 'Edit Profile',
+        path: '/dashboard/editprofile',
+        key: 'profile',
+      },
+      {
+        title: 'Change Password',
+        path: '/dashboard/changepassword',
+        key: 'changepassword',
+      },
+
+    ],
+    path: '/dashboard/AccountSettings',
+  },
+  {
+    title: 'Logout',
+    icon: 'fa fa-sign-out',
+    path: '/logout',
+  },
+];
+const itemsCOMEDIAN = [
+  {
+    title: 'Home',
+    icon: 'fa fa-dashboard',
+    children: [
+      {
+        title: 'Your Statistics ',
+        path: '/dashboard/yourstatistics',
+        key: 'yourstatistics',
+      },
+      {
+        title: 'Video List ',
+        path: '/dashboard/yourvideos',
+        key: 'yourvideos',
+      },
+      {
+        title: 'Staff List ',
+        path: '/dashboard/staff',
+        key: 'staff',
       },
     ],
   },
@@ -148,4 +234,4 @@ const itemsAdmin = [
   },
 
 ];
-export { itemsSuperAdmin, itemsAdmin };
+export { itemsSuperAdmin, itemsAdmin, itemsCOMEDIAN };
