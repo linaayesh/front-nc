1612a23c25d1cfe061374430c3309d04863d4d2e
AymanSami98 <afahgan@students.iugaza.edu.ps>
2022-09-07 14:34:22 +0300

    update the statistics

--- src/pages/UserPages/EndUser/Statistics.page.jsx
+++ src/pages/UserPages/EndUser/Statistics.page.jsx
@@ -8,32 +8,33 @@ function Statistics() {
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState('');
-  const { checkAuth: { user } } = useAppSelector((state) => state);
-  console.log(user);
+  const {
+    checkAuth: { user },
+  } = useAppSelector((state) => state);
+
   useEffect(() => {
-    const fetchData = () => userService.getStatistics(3)
-      .then((res) => setData(res.data))
-      .catch((err) => setError(err.msg))
-      .finally(() => setIsLoading(false));
-    fetchData();
+    if (user.id) {
+      const fetchData = () => userService
+        .getStatistics(user.id)
+        .then((res) => setData(res.data.data))
+        .catch((err) => setError(err.msg))
+        .finally(() => setIsLoading(false));
+      fetchData();
+    }
   }, []);
 
   const { Content, earning } = data;
   if (error) console.log(2222, error);
   if (isLoading) {
-    return (
-      <h2>Loading</h2>
-    );
-  }console.log(Content);
+    return <h2>Loading</h2>;
+  }
   // eslint-disable-next-line no-unsafe-optional-chaining, max-len
-  const balance = +Content?.rows?.reduce((acc, { owedAccRevenue }) => +acc + Number(owedAccRevenue), 0).toFixed(4);
+  const balance = +Content?.rows
+    ?.reduce((acc, { owedAccRevenue }) => +acc + Number(owedAccRevenue), 0)
+    .toFixed(4);
   return (
     <>
-      <GeneralView
-        count={Content.count}
-        balance={balance}
-        earning={+earning}
-      />
+      <GeneralView count={Content.count} balance={balance} earning={+earning} />
       <DetailedView rows={Content.rows} />
     </>
   );
