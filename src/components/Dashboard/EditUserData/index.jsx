dfd7d3d552fe2fcc129a4d25c77c4a8bd88fd891
AymanSami98 <afahgan@students.iugaza.edu.ps>
2022-09-07 10:41:20 +0300

    update the component to show data from backend

--- src/components/Dashboard/EditUserData/index.jsx
+++ src/components/Dashboard/EditUserData/index.jsx
@@ -1,15 +1,22 @@
-import { validationMessages } from 'utils';
-import { Input, Button, Form } from 'components/AntDesign';
-import './style.css';
 import { useParams } from 'react-router-dom';
 import { useAppDispatch } from 'hooks';
-import { editUserProfile } from 'store/admin/thunk';
 import { ComponentLayout } from 'layouts';
+import { validationMessages } from 'utils';
+import { Input, Button, Form } from 'components/AntDesign';
+import { editUserProfile, getUserProfile } from 'store/admin/thunk';
+import { useEffect } from 'react';
 
 function EditUserData() {
   const dispatch = useAppDispatch();
   const [form] = Form.useForm();
   const { userId } = useParams();
+
+  useEffect(() => {
+    (async () => {
+      const { payload } = await dispatch(getUserProfile(userId));
+      form.setFieldsValue(payload);
+    })();
+  }, []);
 
   const onFinish = async (values) => {
     const { name } = values;
@@ -20,18 +27,15 @@ function EditUserData() {
     dispatch(editUserProfile(userUpdatedInfo));
   };
   return (
-    <ComponentLayout title="Edit Profile" heading="Change Your Information">
+    <ComponentLayout heading="Change user profile">
       <Form form={form} name="basic" onFinish={onFinish} autoComplete="off">
         <div className="edit-form-s">
           <Form.Item name="name" rules={validationMessages.name}>
             <Input placeholder="Name" className="edit-form-input" />
           </Form.Item>
-          <Form.Item name="email" rules={validationMessages.email}>
-            <Input placeholder="Email" type="email" />
-          </Form.Item>
         </div>
 
-        <Button className="edit-button" type="primary" htmlType="submit">
+        <Button className="edit-button-admin" type="primary" htmlType="submit">
           Edit Profile
         </Button>
       </Form>
