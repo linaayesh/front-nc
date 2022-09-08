39b4af37f527c88cd61533cbf080ed1e6727365a
AymanSami98 <afahgan@students.iugaza.edu.ps>
2022-09-08 16:28:49 +0300

    update the props

--- src/components/StatisticsGrid/index.jsx
+++ src/components/StatisticsGrid/index.jsx
@@ -4,7 +4,7 @@ import { Card, Statistic } from 'components/AntDesign';
 export default function StatisticsGrid({ icon, title, value }) {
   return (
     <Card.Grid>
-      <img src={icon} alt="icon" style={{ width: '50px' }} />
+      <img src={icon} alt="icon" className="iconimg" />
       <Statistic title={title} value={value} />
     </Card.Grid>
   );
@@ -13,5 +13,5 @@ export default function StatisticsGrid({ icon, title, value }) {
 StatisticsGrid.propTypes = {
   icon: Proptypes.string.isRequired,
   title: Proptypes.string.isRequired,
-  value: Proptypes.number.isRequired,
+  value: Proptypes.string.isRequired,
 };
