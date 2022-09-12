8297a5c016cc1613e32d4d9e1e6ef4eafe631493
RandSohail <randsohail98@gmail.com>
2022-09-12 12:54:03 +0300

    edit content end points, edit user profile by admin

--- src/shared/constants/endpoints/index.js
+++ src/shared/constants/endpoints/index.js
@@ -15,25 +15,27 @@ export const RESET_PASSWORD_URL = `${BASE_URL}/api/v1/auth/reset-password`;
 export const USER_WAITING_LIST_URL = `${BASE_URL}/api/v1/admin/waiting-list`;
 export const USER_APPROVED_LIST_URL = `${BASE_URL}/api/v1/admin/approved-list`;
 export const USER_REJECTED_LIST_URL = `${BASE_URL}/api/v1/admin/rejected-list`;
-export const USER_PANNED_LIST_URL = `${BASE_URL}/api/v1/admin/banned-list`; //! make a page
+export const USER_BANNED_LIST_URL = `${BASE_URL}/api/v1/admin/banned-list`;
 
 export const ADD_USER_URL = `${BASE_URL}/api/v1/admin/add-user`;
 
 export const APPROVE_USER_BY_ID_URL = (userId) => `${BASE_URL}/api/v1/admin/approve/${userId}`;
 export const REJECT_USER_BY_ID_URL = (userId) => `${BASE_URL}/api/v1/admin/reject/${userId}`;
-export const BLOCK_USER_BY_ID_URL = (userId) => `${BASE_URL}/api/v1/admin/block-user/${userId}`; //! add functionality
+export const BLOCK_USER_BY_ID_URL = (userId) => `${BASE_URL}/api/v1/admin/block-user/${userId}`;
 
 export const GET_DASHBOARD_SETTINGS = `${BASE_URL}/api/v1/admin/dashboard-settings`;
 export const UPDATE_DASHBOARD_SETTINGS = `${BASE_URL}/api/v1/admin/edit-dashboard-settings`;
 
 export const GET_USERS_URL = `${BASE_URL}/api/v1/admin/users`;
 
+export const EDIT_USER_PROFILE = `${BASE_URL}/api/v1/admin/edit-user-profile`;
+
 // * content
-export const GET_PAGINATED_CONTENT = ({ limit, page }) => `${BASE_URL}/api/v1/content/contents?limit=${limit}&page=${page}`;
+export const GET_PAGINATED_CONTENT = ({ limit, page }) => `${BASE_URL}/api/v1/content?limit=${limit}&page=${page}`;
 
-export const GET_CONTENT_BY_ID = (contentId) => `${BASE_URL}/api/v1/content/contents?limit=1&page=1&id=${contentId}`;
+export const GET_CONTENT_BY_ID = (contentId) => `${BASE_URL}/api/v1/content?limit=1&page=1&id=${contentId}`;
 
-export const GET_POSSIBLE_CONTENTS = (title) => `${BASE_URL}/api/v1/content/contents?limit=1&page=1&title=${title}`;
+export const GET_POSSIBLE_CONTENTS = (title) => `${BASE_URL}/api/v1/content?limit=1&page=1&title=${title}`;
 
 export const MATCH_USER_CONTENT = `${BASE_URL}/api/v1/content/match-user-content`;
 
@@ -43,13 +45,10 @@ export const GET_STATISTICS = (userId) => `${BASE_URL}/api/v1/user/statistics/${
 export const STATISTICS_DATA_URL = `${BASE_URL}/api/v1/user/statistics`;
 
 export const EDIT_PROFILE_URL = `${BASE_URL}/api/v1/auth/edit-profile`;
-export const EDIT_USER_PROFILE = `${BASE_URL}/api/v1/user/edit-user-profile`;
 
 export const GET_USER_PROFILE = (userId) => `${BASE_URL}/api/v1/user/user-data/${userId}`;
 
 export const FINANCIAL_STATUS_URL = `${BASE_URL}/api/v1/user/financial-information`;
-
-// ! Frontend endpoints
 
 // * navigation endpoints
 export const MATCH_CONTENT_ENDPOINT = (id) => `/dashboard/contentlist/matchcontent/${id}`;
